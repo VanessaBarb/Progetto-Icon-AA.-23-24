c6d8866502191723e1ff89880c0a15793eabd0f9
Stefano Todaro <79905674+StefTodaro@users.noreply.github.com>
2024-08-17 17:44:59 +0200

    Add files via upload

--- dataset_tuning.py
+++ dataset_tuning.py
@@ -3,6 +3,7 @@ import numpy as np
 
 df_t=pd.read_csv(r"C:\Users\Stefano\Desktop\ICON\city_temperature.csv")
 df_a=pd.read_csv(r"C:\Users\Stefano\Desktop\ICON\air_index.csv")
+df_2023=pd.read_csv(r"C:\Users\Stefano\Desktop\ICON\ProgettoIcon\global_air_quality.csv")
 
 
 
@@ -19,7 +20,7 @@ def city_tuning(aqi_data,temperature_data):
     temperature_data = temperature_data[temperature_data['City'].isin(city_set)]
 
     # Salva il DataFrame filtrato in un nuovo file CSV
-    temperature_data.to_csv(r"C:\Users\Stefano\Desktop\ICON\city_temperature_fined1.csv", index=False)
+    temperature_data.to_csv(r"city_temperature_fined1.csv", index=False)
 
     print("done")
 
@@ -35,7 +36,7 @@ def yearly_temperature_avg(temperature_dataset):
 
     annual_mean_temp_per_city.sort_values(by=['Country','City', 'Year'])
     # Salva il nuovo DataFrame in un file CSV
-    output_file_path = r"C:\Users\Stefano\Desktop\ICON\city_temperature_yearly.csv"
+    output_file_path = r"city_temperature_yearly.csv"
     annual_mean_temp_per_city.to_csv(output_file_path, index=False)
 
     return annual_mean_temp_per_city
@@ -54,7 +55,7 @@ def restructure_temperature(temperature_dataset):
     df_pivot.reset_index(inplace=True)
 
     # Salva il DataFrame ristrutturato in un nuovo file CSV
-    output_file_path = r"C:\Users\Stefano\Desktop\ICON\city_temperature_yearly.csv"
+    output_file_path = r"city_temperature_yearly.csv"
     df_pivot.to_csv(output_file_path, index=False)
 
     return df_pivot
@@ -76,7 +77,7 @@ def aqi_data_refinement(aqi_data):
     aqi_data.dropna(inplace=True)
 
     # Salva il DataFrame raffinato in un nuovo file CSV
-    aqi_data.to_csv(r"C:\Users\Stefano\Desktop\ICON\air_index_refined.csv", index=False)
+    aqi_data.to_csv(r"air_index_refined.csv", index=False)
     print("done")
 
     return aqi_data
@@ -89,7 +90,7 @@ def date_restriction(temperature_dataset):
     # Elimina le righe dal DataFrame
     temperature_dataset.drop(indices_to_drop, inplace=True)
 
-    temperature_dataset.to_csv(r"C:\Users\Stefano\Desktop\ICON\city_temperature_fined.csv", index=False)
+    temperature_dataset.to_csv(r"city_temperature_fined.csv", index=False)
 
     print("done")
 
@@ -106,8 +107,36 @@ def fuse_datasets(aqi_dataset,temperature_dataset):
     "2019" : "avg_aqi_2019",
     "2020" : "avg_aqi_2020"})
     merged_df=pd.merge(temperature_dataset,aqi_columns,on="City",how="left")
-    merged_df.to_csv(r"C:\Users\Stefano\Desktop\ICON\yearly_temperature_aqi.csv", index=False)
+    merged_df = merged_df.drop_duplicates(subset='City', keep='first')
+    merged_df.to_csv(r"yearly_temperature_aqi.csv", index=False)
 
+    return merged_df
+
+
+def avg_2023(yearly_dataset,dataset_2023):
+   # Calcola la media della temperatura per ogni città nel 2023
+    city_avg_temp = dataset_2023.groupby(['City'])['Temperature'].mean().reset_index()
+    city_avg_temp.rename(columns={"Temperature": "avg_temperature_2023"}, inplace=True)
+
+    # Calcola la media della qualità dell'aria per ogni città nel 2023
+    city_avg_aqi = dataset_2023.groupby([ 'City'])['Air_Quality'].mean().reset_index()
+    city_avg_aqi.rename(columns={"Air_Quality": "avg_aqi_2023"}, inplace=True)
+
+    # Unisci le due medie calcolate con il dataset originale
+    city_avg = pd.merge(city_avg_temp, city_avg_aqi, on=[ 'City'])
+    # Unisci il dataset annuale con il dataset delle medie del 2023
+    merged_df = pd.merge( yearly_dataset,city_avg, on=["City"], how="left")
+
+    merged_df.replace(r'^\s*$', pd.NA, regex=True, inplace=True)
+
+    # Elimina tutte le righe che contengono NaN (che corrisponde ai valori vuoti sostituiti sopra)
+    merged_df.dropna(inplace=True)
+
+    # Salva il risultato in un file CSV
+    output_file_path = r"final_yearly_temperature_aqi.csv"
+    merged_df.to_csv(output_file_path, index=False)
+
+    return merged_df
 
 
 
@@ -116,6 +145,7 @@ df_t=date_restriction(df_t)
 df_t=city_tuning(df_a,df_t)
 df_t=yearly_temperature_avg(df_t)
 df_t=restructure_temperature((df_t))
-fuse_datasets(df_a,df_t)
+df_y=fuse_datasets(df_a,df_t)
+avg_2023(df_y,df_2023)
 
 
