46d24b8e690f30cb0e4a390eec8c3c675f7858f5
VanessaBarb <79840380+VanessaBarb@users.noreply.github.com>
2024-08-27 00:25:02 +0200

    Aggiunto file .csv bilanciato con la tecnica smote
    Più codice relativo alla tecnica utilizzata

---
+++ Smote.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+
+df= pd.read_csv("globalAirNew.csv")
+
+features=['PM2.5','PM10']
+X= df[features]
+y= df['Air_Quality_Category']
+
+smote= SMOTE(random_state=42)
+X_res, y_res= smote.fit_resample(X,y)
+
+#Creazione di un nuovo dataframe con dati bilanciati
+df_resampled= pd.concat([pd.DataFrame(X_res, columns= features), pd.DataFrame(y_res, columns=['Air_Quality_Category'])], axis=1)
+
+df_resampled.to_csv("globalAirNew_balanced.csv", index= False)
+print("Done..")
